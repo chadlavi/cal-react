72f94091dd31ad9da5d85ff87cae146346e32316
Chad Lavimoniere <chad@chadlavimoniere.com>
2019-06-30 02:05:39 -0400

    simplify unnecessary spread

--- src/theme/cal.theme.ts
+++ src/theme/cal.theme.ts
@@ -3,15 +3,12 @@ import {metrics} from './metrics'
 import {borders} from './borders'
 
 const theme = {
-  color: {
-    ...colors,
-  },
-  borders: {
-    ...borders,
-  },
-  metrics: {
-    ...metrics,
-  },
+  color: colors,
+  
+  borders: borders,
+  
+  metrics: metrics,
+  
   font: {
     fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
     fontSize: metrics.base * 2,
