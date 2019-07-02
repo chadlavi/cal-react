6ef93c3f02c8185e84d0f6c68e1af337a3637553
Chad Lavimoniere <chad@chadlavimoniere.com>
2019-07-01 21:17:26 -0400

    add breakpoints

--- src/theme/metrics.ts
+++ src/theme/metrics.ts
@@ -11,12 +11,23 @@ const padding = {
   large: base * 3,
 }
 
+enum Breakpoint {
+  sm = 578,
+  m = 768,
+  l = 992,
+  xl = 1200
+}
+
+const over = (size: Breakpoint) => `@media (min-width: ${size}px)`
+const under = (size: Breakpoint) => `@media (max-width: ${size + 1}px)`
+
 export const metrics = {
   base: base,
-  spacing: {
-    ...spacing,
-  },
-  padding: {
-    ...padding,
+  spacing: spacing,
+  padding: padding,
+  breakpoints: Breakpoint,
+  helpers: {
+    over: over,
+    under: under,
   }
 }
