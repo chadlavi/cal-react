eec3e74268ce1b0ee8cb5c7b821805c38efec48f
Chad Lavimoniere <chad@chadlavimoniere.com>
2019-07-01 21:17:12 -0400

    add fontawesome

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  IconName,
+  library,
+} from '@fortawesome/fontawesome-svg-core'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+enum IconTypes {
+  solid = 'fas',
+  regular = 'far',
+  brand = 'fab',
+}
+
+interface IconProps {
+  name: IconName
+  type?: 'solid' | 'regular' | 'brand'
+}
+
+export const Icon = (props: IconProps) => {
+
+  const {
+    type = 'solid',
+    name
+  } = props
+
+return <FontAwesomeIcon icon={[IconTypes[type], name]} />
+}
